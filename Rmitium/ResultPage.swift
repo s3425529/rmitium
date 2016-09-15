77063930e3ae42c50fd34d05f764bfae8aeb8671
Max <Max@maxs-macbook-air.local>
2016-09-15 14:57:43 +1000

    level1 result page

--- Rmitium/ResultPage.swift
+++ Rmitium/ResultPage.swift
@@ -23,11 +23,39 @@ import Social
 class ResultPage: SKScene{
     
     var facebook, twitter, redo, next: CustomButton!
-    
-    
+
+    var homeDialogue,homeView :SKSpriteNode!
+     
     
     override func didMoveToView(view: SKView) {
         
+        let levelLabel = SKLabelNode(fontNamed:UtilitiesPortal.navLabelFont)
+        levelLabel.zPosition = 0.1
+        levelLabel.text = UtilitiesPortal.levelLabelTexts[0]
+        levelLabel.fontSize = UtilitiesPortal.navLabelSize
+        levelLabel.position = CGPointMake(frame.midX, UtilitiesPortal.screenHeight*0.92)
+        self.addChild(levelLabel)
+        
+        // Home button
+        let home = SKSpriteNode(imageNamed: "home")
+        home.name = UtilitiesPortal.homeButtonName
+        home.zPosition = 0.1
+        home.alpha = 1
+        home.size = CGSize(width: UtilitiesPortal.navImgSize, height: UtilitiesPortal.navImgSize)
+        home.position = CGPoint(x:UtilitiesPortal.borderSize/2,
+                                y:UtilitiesPortal.screenHeight - UtilitiesPortal.navImgSize/2)
+        addChild(home)
+        
+        // Info button
+        let info = SKSpriteNode(imageNamed: "help2")
+        info.name = UtilitiesPortal.infoButonName
+        info.zPosition = 0.1
+        info.alpha = 1
+        info.size = CGSize(width: UtilitiesPortal.navImgSize, height: UtilitiesPortal.navImgSize)
+        info.position = CGPoint(x:UtilitiesPortal.screenWidth - UtilitiesPortal.borderSize/2,
+                                y:UtilitiesPortal.screenHeight - UtilitiesPortal.navImgSize/2)
+        addChild(info)
+
         
         facebook = CustomButton(defaultButtonImage: "facebookbutton", activeButtonImage: "facebookbutton1", buttonAction: facebookAction,scale: 0.2)
         facebook.position = CGPoint(x:UtilitiesPortal.screenWidth - UtilitiesPortal.borderSize*3,
@@ -65,7 +93,7 @@ class ResultPage: SKScene{
         scoreNode.text = "\(UtilitiesPortal.score)"
         scoreNode.position = CGPoint(x: UtilitiesPortal.screenWidth*0.3, y: UtilitiesPortal.screenHeight*0.5)
         addChild(scoreNode)
-        
+        createHomeDialogue()
     }
     func facebookAction(){
         print("facebook")
@@ -108,7 +136,8 @@ class ResultPage: SKScene{
         backHomePage()
     }
     
-    
+   
+
     
     
     // Share the score to any social media!
@@ -142,6 +171,81 @@ class ResultPage: SKScene{
         
     }
     
+    func createHomeDialogue() {
+        homeView  = SKSpriteNode()
+        homeView.size = CGSize(width: UtilitiesPortal.screenWidth, height: UtilitiesPortal.screenHeight)
+        homeView.position = CGPoint(x: UtilitiesPortal.screenWidth/2.5, y: UtilitiesPortal.screenHeight/2)
+        homeView.zPosition = 0.8
+        // homeView.color = SKColor.redColor()
+        homeView.alpha = 1
+        homeView.hidden = true
+        let yesBtn = SKSpriteNode()
+        let noBtn = SKSpriteNode()
+        
+        let alertMessage = SKLabelNode(text: "Are you sure you want to quit?")
+        alertMessage.position = CGPoint(x: 0, y: 0)
+        alertMessage.zPosition = 0.9
+        alertMessage.fontName = UtilitiesPortal.navLabelFont
+        alertMessage.fontSize = 15
+        homeDialogue = SKSpriteNode()
+        homeDialogue.size = CGSize(width: UtilitiesPortal.screenWidth/2.5, height: UtilitiesPortal.screenHeight/2.5)
+        homeDialogue.position = CGPoint(x: UtilitiesPortal.screenWidth/11, y: UtilitiesPortal.screenHeight/11)
+        homeDialogue.color = SKColor.blackColor()
+        homeDialogue.alpha = 0.9
+        homeDialogue.zPosition = 0.9
+        homeDialogue.hidden = false
+        
+        yesBtn.size = CGSize(width: UtilitiesPortal.navImgSize, height: UtilitiesPortal.navImgSize)
+        yesBtn.color = SKColor.grayColor()
+        yesBtn.name = UtilitiesPortal.yesButtonName
+        yesBtn.texture = SKTexture(imageNamed: "tick-white")
+        yesBtn.position = CGPoint(x: (0 - yesBtn.size.width), y: (0 - yesBtn.size.height)*1.5)
+        yesBtn.zPosition = 0.9
+        
+        noBtn.size = yesBtn.size
+        noBtn.color = yesBtn.color
+        noBtn.name = UtilitiesPortal.noButtonName
+        noBtn.texture = SKTexture(imageNamed: "cross-white")
+        noBtn.position = CGPoint(x: yesBtn.size.width, y: (0 - yesBtn.size.height)*1.5)
+        noBtn.zPosition = 0.9
+        
+        homeDialogue.addChild(yesBtn)
+        homeDialogue.addChild(noBtn)
+        homeDialogue.addChild(alertMessage)
+        homeView.addChild(homeDialogue)
+        //addChild(homeDialogue)
+        addChild(homeView)
+        
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch = touches.first
+        
+        // Home button selected
+        let location = touch!.locationInNode(self)
+        let node = self.nodeAtPoint(location)
+        if node.name == UtilitiesPortal.homeButtonName {
+            print("Home!")
+            homeView.hidden = false
+            
+            //homeDialogue.hidden = false
+          
+        }
+        if node.name == UtilitiesPortal.yesButtonName {
+            backHomePage()
+            return
+        }
+        if node.name == UtilitiesPortal.noButtonName {
+            //homeDialogue.hidden = true
+            homeView.hidden = true
+          
+            return
+        }
+        
+        
+        
+        
+    }
     
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
